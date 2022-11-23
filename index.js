01b021097b5d0f9dc96bab5cb42026309e0b6119
ombatied <ombatied@gmail.com>
2022-11-23 20:44:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const people = ["Ada", "Brendan", "Ali"]
+
+function writeCards(people) {
+    let messages = [];
+    for  (let i = 0; i < people.length; i++) {
+        messages.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(people));
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+console.log(countDown(10));
